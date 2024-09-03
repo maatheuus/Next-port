4ed98db3b60303412154b06801c78fdb91f43eb4
MATHEUS <matts14smkd@gmail.com>
2024-09-03 08:29:59 -0300

    Update Providers.js

--- app/Providers.js
+++ app/Providers.js
@@ -1,16 +1,9 @@
 "use client";
-// import { useEffect, useState } from "react";
 import { ModalProvider } from "@/context/ModalContext";
 import { MenuProvider } from "@/context/MenuContext";
 import { ThemeProvider } from "./theme-provider";
 
 function Providers({ children }) {
-  // const [mounted, setMounted] = useState(false);
-
-  // useEffect(() => setMounted(true), [mounted]);
-
-  // if (!mounted) return <ModalProvider>{children}</ModalProvider>;
-
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <MenuProvider>
